d6bdff7d2d294bba423bb0b8b5ac4a92cb9803f5
valongueiro <andre.valongueiro@gmail.com>
2021-11-08 09:51:47 +0700

    testing sbasic features.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import MainHeader from "./components/MainHeader";
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
@@ -9,15 +9,20 @@ function App() {
     <div>
       <MainHeader />
       <main>
-        <Route path="/welcome">
-          <Welcome />
-        </Route>
-        <Route path="/products">
-          <Products />
-        </Route>
-        <Route path="/product/:country">
-          <ProductDetail />
-        </Route>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/welcome" />
+          </Route>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          <Route path="/products/:country">
+            <ProductDetail />
+          </Route>
+        </Switch>
       </main>
     </div>
   );
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,11 +1,19 @@
+import { Link } from "react-router-dom";
+
 const Products = () => {
   return (
     <section>
       <h1>The Products Page</h1>
       <ul>
-        <li>A Trip To Thailand</li>
-        <li>A Trip To Philippines</li>
-        <li>A Trip To Brazil</li>
+        <li>
+          <Link to="/products/thailand">A Trip To Thailand</Link>
+        </li>
+        <li>
+          <Link to="/products/philippines">A Trip To Philippines</Link>
+        </li>
+        <li>
+          <Link to="/products/brazil">A Trip To Brazil</Link>
+        </li>
       </ul>
     </section>
   );
